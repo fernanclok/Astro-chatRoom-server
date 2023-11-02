8571cb273154f3713bb532da54431ca9ff2d2a0f
Fernando Medina <fernanclok@gmail.com>
2023-11-02 03:50:18 +0000

    funcionando

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const { Server } = require("socket.io");
 
 require("dotenv").config();
@@ -17,19 +18,18 @@ app.use(
 );
 
 app.use(express.json());
-const server = http.createServer(app);
+const server = https.createServer(
+  {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+  },app);
 const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
+  connectionStateRecovery: {}
 });
-
-// Incluye esta línea para servir la biblioteca Socket.io
-app.use(
-  "/socket.io",
-  express.static(__dirname + "/node_modules/socket.io/client-dist")
-);
 
 // importar rutas
 const userRoutes = require("./routes/userRoutes");
@@ -49,31 +49,15 @@ app.use("/api", messageRoutes);
 io.on("connection", (socket) => {
   console.log("Cliente conectado");
 
-  socket.on("join room", (roomId) => {
-    socket.join(roomId);
-    console.log(`Cliente se ha unido a la sala: ${roomId}`);
-
-  });
-
-  socket.on('chat message', async (messageData) => {
-    console.log("mensaje recibido", messageData);
-    try {
-      const messageId = await messageModel.createMessage(messageData);
-      messageData.id = messageId;
-  
-      // Emitir el mensaje a todos los usuarios en la sala
-      io.to(messageData.room_id).emit('message', messageData);
-  
-      console.log("mensaje emitido a todos los usuarios en la sala", messageData);
-    } catch (error) {
-      console.error('Error al guardar el mensaje en la base de datos:', error);
-    }
-  });
-
   socket.on("disconnect", () => {
     console.log("Cliente desconectado");
   });
-});;
+
+  socket.on('chat message', async (messageData) => {
+    console.log('Mensaje recibido en el servidor:', messageData);
+    io.emit('chat message', messageData)
+  })
+});
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
